ee11f30da0e9720ade1caaaec88cba3a6a688926
Varad Rampurikar <103248983+rvarad@users.noreply.github.com>
2025-02-11 12:55:22 +0000

    Pending changes exported from your codespace

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["solid-capybara-r55x65w9vw4hpg9r-3000.app.github.dev"]
+        }
+    }
+};
 
 export default nextConfig;
